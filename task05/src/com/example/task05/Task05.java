04f825b15fecb053aed2d91037b41356032aca98
Elaiza-art <liza.kupidonli25@mail.ru>
2025-09-15 14:54:21 +0500

    аличина елизавета при-202 – задание 5

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,18 +4,26 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        String arg = String.valueOf(x);
 
-        return "FALSE";
+        for (int i = 0; i < arg.length(); i++ ){
+            char charNumb = arg.charAt(i);
+            int number = Character.getNumericValue(charNumb);
+
+            if (number % 2 != 0){
+                return "FALSE";
+            }
+        }
+
+        return "TRUE";
+
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         String result = solution(1234);
         System.out.println(result);
-        */
     }
 
 }
