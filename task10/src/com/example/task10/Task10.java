6207c487ab4af3826e35ca2a38606994060b71b6
Elaiza-art <liza.kupidonli25@mail.ru>
2025-09-15 17:40:19 +0500

    Галичина Елизавета. Задание 10

--- task10/src/com/example/task10/Task10.java
+++ task10/src/com/example/task10/Task10.java
@@ -7,10 +7,22 @@ public class Task10 {
         // TODO корректно сравнивать два значения типа float с заданной пользователем точностью (параметр - количество знаков после запятой).
         // Функция должна корректно обрабатывать ситуацию со сравнением значений бесконечности.
         // Функция должна считать значения «не число» NaN (например 0.0/0.0) равными между собой.
-        
-        return a == b;
 
-    }
+        if (Float.isInfinite(a) || Float.isInfinite(b)) {
+            return a == b;
+        }
+
+        if (Float.isNaN(a) && Float.isNaN(b)) {
+            return true;
+        }
+        if (Float.isNaN(a) || Float.isNaN(b)) {
+            return false;
+        }
+
+        float E = (float) Math.pow(10, -precision);
+        return Math.abs(a - b) < E;
+
+        }
 
     public static void main(String[] args) {
         float a = 0.3f;
